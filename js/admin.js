5287e6004b2e038a76914770b0a7db31eaa1061d
VoidYeezy <156734366+ott0111@users.noreply.github.com>
2026-03-01 23:27:59 -0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,32 @@
+const table = document.getElementById("applicationsTable")
 
+let user = localStorage.getItem("voidUser")
+let score = localStorage.getItem("voidScore")
+
+if(user){
+
+let status = score >= 20 ? "PASS" : "FAIL"
+
+let row = document.createElement("tr")
+
+row.innerHTML = `
+
+<td>${user}</td>
+
+<td>${score} / 30</td>
+
+<td class="${score >= 20 ? "pass" : "fail"}">${status}</td>
+
+<td>
+
+<button class="accept-btn">Accept</button>
+
+<button class="deny-btn">Deny</button>
+
+</td>
+
+`
+
+table.appendChild(row)
+
+}
